fb88dc903d007cd3ac5518845e415a9153b67f0d
Aaron <aeroyts@icloud.com>
2020-07-03 14:32:37 +0100

    Homepage view and Components created
    A homepage view which is routes to "/home" has been created and made. It includes components which we have added for different sections of the home view.
    
    In this commit we have completed the lesson: Design the Timeline

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,12 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img src="https://i.pravatar.cc/50" alt="" class="rounded-full mr-2">
+    </div>
+    <div>
+        <h5 class="font-bold mb-4">John Doe</h5>
+        <p>
+            Presumably, if you're watching this series, you've already made the decision to embrace all that Laravel has to offer.
+            However, if you're still on the fence, give me just a moment to sell you on why I believe Laravel is the best framework choice in the PHP world.
+        </p>
+    </div>
+</div>
