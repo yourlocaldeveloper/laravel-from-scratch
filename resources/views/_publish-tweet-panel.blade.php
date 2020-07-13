12e554c803500b386fbc3f98582f87a0cb6d99d8
Aaron <aeroyts@icloud.com>
2020-07-13 17:45:31 +0100

    Dynamic Timeline Added
    Can now send a "tweet" using the form. Timeline is dynamic with basic functionality. Edited views and corrected avatars to be unique to each "user."

--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -1,10 +1,15 @@
 <div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
-    <form action="">
+    <form method="POST" action="/tweets">
+        @csrf
         <textarea name="body" class="w-full" placeholder="What's up doc!?"></textarea>
         <hr class="my-4">
         <footer class="flex justify-between">
-            <img src="https://i.pravatar.cc/40" alt="" class="rounded-full mr-2">
+            <img src="{{ auth()->user()->avatar }}" alt="your avatar" class="rounded-full mr-2">
             <button type="submit" class="bg-blue-500 rounded-lg shadow p-2 text-white">Tweet-a-roo!</button>
         </footer>
     </form>
+
+    @error('body')
+        <p class="text-red-500 text-sm mt-2">{{ $message }}</p>
+    @enderror
 </div>
